a26f47d3c42432f34e6f009e5e2821523015e7e3
x14g017 <x14g017@PC505N016.chiba-fjb.ac.jp>
2015-04-16 15:02:01 +0900

    ぽいぽいぽーい

---
+++ src/poipoi/Format.java
@@ -0,0 +1,17 @@
+package poipoi;
+
+public class Format {
+
+	public static void main(String[] args) {
+
+		int[] a = {5,8,2,1,4};
+		int i;
+		for(i=0;i<a.length;i++)
+		{
+		    System.out.println(a[i]);
+		}
+
+
+	}
+
+}
